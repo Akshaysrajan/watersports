fe4bde15e734f50e5f8c269e072f5f93dae821f3
Akshay S Rajan <76043412+Akshaysrajan@users.noreply.github.com>
2025-09-01 18:12:20 +0530

    description removed

--- src/data/activities.ts
+++ src/data/activities.ts
@@ -5,42 +5,48 @@ export const activities: Activity[] = [
   {
     id: "jet-ski",
     title: "Jet Skiing",
-    description: "Experience the thrill of speeding across Kerala's beautiful backwaters on our high-powered jet skis. Perfect for adrenaline seekers!",
+    // description: "Experience the thrill of speeding across Kerala's beautiful backwaters on our high-powered jet skis. Perfect for adrenaline seekers!",
+    description: "Coming Soon!",
     price: 1500,
     image: user2
   },
   {
     id: "Sailing",
     title: "Sailing",
-    description: "Soar high above the Arabian Sea and enjoy breathtaking views of Kerala's coastline with our safe and exciting parasailing experience.",
+    // description: "Soar high above the Arabian Sea and enjoy breathtaking views of Kerala's coastline with our safe and exciting parasailing experience.",
+    description : "",
     price: 2000,
     image: "https://images.unsplash.com/photo-1613578699399-82ae71be53a3?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     id: "banana-boat",
     title: "Water Bicycle",
-    description: "Hop on our inflatable banana boat with friends and family for a fun-filled ride that's guaranteed to bring laughs and excitement.",
+    // description: "Hop on our inflatable banana boat with friends and family for a fun-filled ride that's guaranteed to bring laughs and excitement.",
+    description : "",
     price: 800,
     image: "https://images.pexels.com/photos/1430672/pexels-photo-1430672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   },
   {
     id: "kayaking",
     title: "Kayaking",
-    description: "Explore Kerala's serene backwaters at your own pace with our single and tandem kayaks. Perfect for nature lovers and photography enthusiasts.",
+    // description: "Explore Kerala's serene backwaters at your own pace with our single and tandem kayaks. Perfect for nature lovers and photography enthusiasts.",
+    description : "",
     price: 1000,
     image: "https://plus.unsplash.com/premium_photo-1661893427047-16f6ddc173f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     id: "stand-up-paddle",
     title: "Stand-Up Paddleboarding",
-    description: "Find your balance and peace on our stand-up paddleboards while taking in the beautiful surroundings of Kerala's calm waters.",
+    // description: "Find your balance and peace on our stand-up paddleboards while taking in the beautiful surroundings of Kerala's calm waters.",
+    description : "",
     price: 1200,
     image: "https://media.istockphoto.com/id/1060154310/photo/summer-sunset-lake-paddleboarding-detail.webp?s=2048x2048&w=is&k=20&c=IUlCwRBowkZCX6fmDOV8QUsO50GV7sl8s2ZR-pVgyoE="
   },
   {
     id: "Power-Boating",
     title: "Power Boating",
-    description: "Feel the rush across the water surface pulled by our speedboats. Our instructors ensure a safe and thrilling experience.",
+    // description: "Feel the rush across the water surface pulled by our speedboats. Our instructors ensure a safe and thrilling experience.",
+    description : "",
     price: 1800,
     image: pb
   }
